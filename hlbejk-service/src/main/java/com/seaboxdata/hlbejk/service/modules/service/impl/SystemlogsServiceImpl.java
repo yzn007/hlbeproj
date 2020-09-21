38737c9c356ead8a010b5f7fd3b8776ee4a98487
wallet88 <wallet88@163.com>
2020-09-21 17:03:12 +0800

    日志按照名称搜索

--- hlbejk-service/src/main/java/com/seaboxdata/hlbejk/service/modules/service/impl/SystemlogsServiceImpl.java
+++ hlbejk-service/src/main/java/com/seaboxdata/hlbejk/service/modules/service/impl/SystemlogsServiceImpl.java
@@ -1,12 +1,19 @@
 package com.seaboxdata.hlbejk.service.modules.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.seaboxdata.hlbejk.api.vo.DaymonitorVO;
+import com.seaboxdata.hlbejk.api.vo.SystemlogsVO;
 import com.seaboxdata.hlbejk.service.modules.dao.SystemlogsDao;
+import com.seaboxdata.hlbejk.service.modules.entity.Daymonitor;
 import com.seaboxdata.hlbejk.service.modules.entity.Systemlogs;
 import com.seaboxdata.hlbejk.service.modules.service.SystemlogsService;
 import com.seaboxdata.commons.core.util.api.PageUtils;
 import com.seaboxdata.commons.core.util.api.Query;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -18,10 +25,16 @@ public class SystemlogsServiceImpl extends ServiceImpl<SystemlogsDao, Systemlogs
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+
+        if(null!=params && null!=params.get("key")&&!StringUtils.isEmpty(params.get("key").toString())){
+            queryWrapper.like("model",params.get("key"));
+        }
         IPage<Systemlogs> page = this.page(
                 new Query<Systemlogs>().getPage(params),
-                new QueryWrapper<>()
+                queryWrapper
         );
+
 
         return new PageUtils(page);
     }
